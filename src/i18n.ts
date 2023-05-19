34e2777d3b10faf9b1aa3053574e71383670daab
NicolasMerget <Nicolas.Merget@deutschebahn.com>
2023-05-20 00:55:21 +0200

    fix: issue with i18next and github pages

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,14 +1,17 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import Backend from 'i18next-http-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import Backend from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
 
 i18n
   .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: 'en',
+    backend: {
+      loadPath: "./locales/{{lng}}/{{ns}}.json",
+    },
+    fallbackLng: "en",
     keySeparator: false, // we do not use keys in form messages.welcome
     interpolation: {
       escapeValue: false, // react already safes from xss
